8659b5cf3bd1e5f04c356bd21fc35ce13dc94691
20020899-debug <20020899@vnu.edu.vn>
2026-08-04 15:50:17 +0700

    Add files via upload

---
+++ TG-Model-Preorder/database.py
@@ -0,0 +1,45 @@
+import sqlite3
+
+conn = sqlite3.connect("orders.db")
+
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS orders(
+
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+    order_code TEXT,
+
+    fullname TEXT,
+
+    phone TEXT,
+
+    contact TEXT,
+
+    province TEXT,
+
+    district TEXT,
+
+    ward TEXT,
+
+    address_detail TEXT,
+
+    quantity INTEGER,
+
+    note TEXT,
+
+    product TEXT,
+
+    deposit INTEGER,
+
+    status TEXT
+
+)
+""")
+
+conn.commit()
+
+conn.close()
+
+print("Database đã tạo.")
