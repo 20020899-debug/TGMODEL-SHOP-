4b8f7bd44e5a6173897a3f10e6fe208a1f2d7582
20020899-debug <20020899@vnu.edu.vn>
2026-08-04 13:30:54 +0700

    Update app.py

--- TG-Model-Preorder/app.py
+++ TG-Model-Preorder/app.py
@@ -19,3 +19,6 @@ if __name__ == "__main__":
 @app.route("/")
 def home():
     return render_template("index.html", product=product)
+@app.route("/preorder")
+def preorder():
+    return render_template("preorder.html", product=product)
