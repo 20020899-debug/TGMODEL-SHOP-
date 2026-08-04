99e389c186078772c36720a89c2b5c8300430a47
20020899-debug <20020899@vnu.edu.vn>
2026-08-04 15:25:57 +0700

    Update address.js

--- TG-Model-Preorder/static/js/address.js
+++ TG-Model-Preorder/static/js/address.js
@@ -1,27 +1,53 @@
-console.log("address.js đã chạy");
-
 const provinceSelect = document.getElementById("province");
+const wardSelect = document.getElementById("ward");
 
+// Tải danh sách tỉnh
 fetch("https://provinces.open-api.vn/api/p/")
-    .then(response => response.json())
+    .then(res => res.json())
     .then(data => {
-
-        console.log(data);
-
         data.forEach(province => {
-
             const option = document.createElement("option");
-
             option.value = province.code;
             option.textContent = province.name;
-
             provinceSelect.appendChild(option);
-
         });
-
-    })
-    .catch(error => {
-
-        console.error(error);
-
     });
+
+// Khi chọn tỉnh
+provinceSelect.addEventListener("change", function () {
+
+    wardSelect.innerHTML =
+        '<option value="">-- Chọn phường / xã --</option>';
+
+    wardSelect.disabled = true;
+
+    if (!this.value) return;
+
+    fetch(`https://provinces.open-api.vn/api/p/${this.value}?depth=2`)
+        .then(res => res.json())
+        .then(data => {
+
+            // API có thể trả về wards hoặc districts tùy phiên bản
+            let wards = [];
+
+            if (data.wards) {
+                wards = data.wards;
+            } else if (data.districts) {
+                data.districts.forEach(d => {
+                    if (d.wards) {
+                        wards.push(...d.wards);
+                    }
+                });
+            }
+
+            wards.forEach(ward => {
+                const option = document.createElement("option");
+                option.value = ward.code;
+                option.textContent = ward.name;
+                wardSelect.appendChild(option);
+            });
+
+            wardSelect.disabled = false;
+        })
+        .catch(err => console.error(err));
+});
