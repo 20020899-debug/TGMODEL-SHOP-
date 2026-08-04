5807795076ee12fe6ae723e5cf89ac7cd2bfcfe8
20020899-debug <20020899@vnu.edu.vn>
2026-08-04 13:27:16 +0700

    Update app.py

--- TG-Model-Preorder/app.py
+++ TG-Model-Preorder/app.py
@@ -3,11 +3,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 product = {
-    "name": "AZURE FALCON",
-    "price": 2390000,
-    "deposit": 500000,
-    "arrival": "Tháng 8/2026",
-    "description": "Phiên bản Pre-order chính hãng."
+    "brand": " IN ERA+",
+    "name": " IN ERA+ TR-2243EX AZURE FALCON 1/72 ",
+    "price": 1300000,
+    "deposit": 300000,
+    "eta": "Tháng 9/2026"
 }
 
 @app.route("/")
