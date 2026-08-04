aa54fdbe3d06e2ddb3046f530c9bbf7b5761dafe
20020899-debug <20020899@vnu.edu.vn>
2026-08-04 15:46:12 +0700

    Update app.py

--- TG-Model-Preorder/app.py
+++ TG-Model-Preorder/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
+# Thông tin sản phẩm đang Pre-order
 product = {
     "brand": "IN ERA+",
     "name": "IN ERA+ TR-2243EX AZURE FALCON 1/72",
@@ -9,36 +11,58 @@ product = {
     "eta": "Tháng 9/2026"
 }
 
+
+# Trang chủ
 @app.route("/")
 def home():
     return render_template("index.html", product=product)
 
+
+# Trang Pre-order
 @app.route("/preorder")
 def preorder():
     return render_template("preorder.html", product=product)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+# Nhận dữ liệu từ form
 @app.route("/submit", methods=["POST"])
 def submit():
 
-    fullname = request.form["fullname"]
-    phone = request.form["phone"]
-    facebook = request.form["facebook"]
-    province = request.form["province"]
-    district = request.form["district"]
-    ward = request.form["ward"]
-    address_detail = request.form["address_detail"]
-    quantity = request.form["quantity"]
-    note = request.form["note"]
+    fullname = request.form.get("fullname")
+    phone = request.form.get("phone")
+    contact = request.form.get("contact")
+    quantity = request.form.get("quantity")
+
+    province = request.form.get("province")
+    district = request.form.get("district")
+    ward = request.form.get("ward")
+    address_detail = request.form.get("address_detail")
+
+    note = request.form.get("note")
 
     return f"""
-    <h2>Đã nhận đơn</h2>
+    <h2>Đặt Pre-order thành công!</h2>
 
-    Tên: {fullname}<br>
-    SĐT: {phone}<br>
-    Facebook/Zalo: {facebook}<br>
-    Địa chỉ: {address_detail}, {ward}, {district}, {province}<br>
-    Số lượng: {quantity}<br>
-    Ghi chú: {note}
+    <hr>
+
+    <b>Họ tên:</b> {fullname}<br>
+    <b>Số điện thoại:</b> {phone}<br>
+    <b>Facebook/Zalo:</b> {contact}<br>
+    <b>Địa chỉ:</b> {address_detail}, {ward}, {district}, {province}<br>
+    <b>Số lượng:</b> {quantity}<br>
+    <b>Ghi chú:</b> {note}<br>
+
+    <hr>
+
+    <h3>Sản phẩm đã đặt</h3>
+
+    <b>Hãng:</b> {product["brand"]}<br>
+    <b>Tên:</b> {product["name"]}<br>
+    <b>Giá:</b> {product["price"]:,} đ<br>
+    <b>Tiền cọc:</b> {product["deposit"]:,} đ<br>
+    <b>Dự kiến trả hàng:</b> {product["eta"]}<br>
     """
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
