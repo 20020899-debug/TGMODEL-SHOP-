7ad7c30588ef87869bf7f7ff90d8bea76594b2d4
20020899-debug <20020899@vnu.edu.vn>
2026-08-04 15:39:13 +0700

    Update app.py

--- TG-Model-Preorder/app.py
+++ TG-Model-Preorder/app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 product = {
@@ -20,3 +19,26 @@ def preorder():
 
 if __name__ == "__main__":
     app.run(debug=True)
+@app.route("/submit", methods=["POST"])
+def submit():
+
+    fullname = request.form["fullname"]
+    phone = request.form["phone"]
+    facebook = request.form["facebook"]
+    province = request.form["province"]
+    district = request.form["district"]
+    ward = request.form["ward"]
+    address_detail = request.form["address_detail"]
+    quantity = request.form["quantity"]
+    note = request.form["note"]
+
+    return f"""
+    <h2>Đã nhận đơn</h2>
+
+    Tên: {fullname}<br>
+    SĐT: {phone}<br>
+    Facebook/Zalo: {facebook}<br>
+    Địa chỉ: {address_detail}, {ward}, {district}, {province}<br>
+    Số lượng: {quantity}<br>
+    Ghi chú: {note}
+    """
