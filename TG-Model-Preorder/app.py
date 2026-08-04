27a70189b6da32442c91dece0d51e49a40bb3c2b
20020899-debug <20020899@vnu.edu.vn>
2026-08-04 13:21:09 +0700

    Update app.py

--- TG-Model-Preorder/app.py
+++ TG-Model-Preorder/app.py
@@ -2,9 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+product = {
+    "name": "MS GENERAL FALCON",
+    "price": 2390000,
+    "deposit": 500000,
+    "arrival": "Tháng 11/2026",
+    "description": "Phiên bản Pre-order chính hãng."
+}
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", product=product)
 
 if __name__ == "__main__":
     app.run(debug=True)
