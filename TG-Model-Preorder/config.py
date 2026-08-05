aaf5bcaa3d78dd8e4f4a2a4157dd3da5f86b5de0
20020899-debug <20020899@vnu.edu.vn>
2026-08-05 10:47:07 +0700

    Update config.py

--- TG-Model-Preorder/config.py
+++ TG-Model-Preorder/config.py
@@ -3,3 +3,12 @@ SECRET_KEY = "TGMODELSHOP"
 ADMIN_USERNAME = "admin"
 
 ADMIN_PASSWORD = "123456"
+
+
+product = {
+    "brand": "IN ERA+",
+    "name": "IN ERA+ TR-2243EX AZURE FALCON 1/72",
+    "price": 1300000,
+    "deposit": 300000,
+    "eta": "Tháng 9/2026"
+}
