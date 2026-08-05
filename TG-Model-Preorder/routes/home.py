804af2fa530291fc6b5144d191b60e786b2835a7
20020899-debug <20020899@vnu.edu.vn>
2026-08-05 10:49:46 +0700

    Update home.py

--- TG-Model-Preorder/routes/home.py
+++ TG-Model-Preorder/routes/home.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+import sqlite3
 
 
 home_bp = Blueprint(
@@ -10,14 +11,28 @@ home_bp = Blueprint(
 @home_bp.route("/")
 def home():
 
+    conn = sqlite3.connect("orders.db")
+    conn.row_factory = sqlite3.Row
+
+    cursor = conn.cursor()
+
+
+    cursor.execute("""
+        SELECT *
+        FROM products
+        WHERE status='Hiện'
+        ORDER BY id DESC
+        LIMIT 1
+    """)
+
+
+    product = cursor.fetchone()
+
+
+    conn.close()
+
+
     return render_template(
-        "index.html"
-    )
-
-
-@home_bp.route("/preorder")
-def preorder():
-
-    return render_template(
-        "preorder.html"
+        "index.html",
+        product=product
     )
