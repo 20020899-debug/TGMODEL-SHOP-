67b8d5e9cf6f66d911c964418c6e3c2d4720b6ec
20020899-debug <20020899@vnu.edu.vn>
2026-08-05 10:33:21 +0700

    Update app.py

--- TG-Model-Preorder/app.py
+++ TG-Model-Preorder/app.py
@@ -18,7 +18,5 @@ app.register_blueprint(admin_bp)
 app.register_blueprint(products_bp)
 
 if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=5000
-    )
+
+    app.run(debug=True)
