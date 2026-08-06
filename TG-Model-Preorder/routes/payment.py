6b3fdf87111eb6951e18d52ff7d2bd096decae75
20020899-debug <20020899@vnu.edu.vn>
2026-08-06 08:59:47 +0700

    Update payment.py

--- TG-Model-Preorder/routes/payment.py
+++ TG-Model-Preorder/routes/payment.py
@@ -20,3 +20,40 @@ def payment_cancel():
     return render_template(
         "payment_cancel.html"
     )
+@payment_bp.route("/payment/webhook", methods=["POST"])
+def webhook():
+
+    data = request.json
+
+    order_code = data["data"]["orderCode"]
+
+    status = data["code"]
+
+
+    if status == "00":
+
+        conn = sqlite3.connect(
+            "orders.db"
+        )
+
+        cursor = conn.cursor()
+
+
+        cursor.execute(
+            """
+            UPDATE orders
+            SET status=?
+            WHERE order_code=?
+            """,
+            (
+                "Đã cọc",
+                f"TG{order_code:06d}"
+            )
+        )
+
+
+        conn.commit()
+        conn.close()
+
+
+    return "OK"
