3cef2e7cfb9608b876b07097b90205f8e4c181bf
20020899-debug <20020899@vnu.edu.vn>
2026-08-06 08:00:06 +0700

    Update app.py

--- TG-Model-Preorder/app.py
+++ TG-Model-Preorder/app.py
@@ -12,11 +12,13 @@ from routes.home import home_bp
 from routes.auth import auth_bp
 from routes.preorder import preorder_bp
 from routes.admin import admin_bp
+from routes.payment import payment_bp
 
 app.register_blueprint(home_bp)
 app.register_blueprint(auth_bp)
 app.register_blueprint(preorder_bp)
 app.register_blueprint(admin_bp)
+app.register_blueprint(payment_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
