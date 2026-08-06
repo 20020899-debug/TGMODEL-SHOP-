70c48a3f7668ad62c4b53088d26746210ef406b0
20020899-debug <20020899@vnu.edu.vn>
2026-08-06 08:47:23 +0700

    Delete TG-Model-Preorder/payment.py

--- TG-Model-Preorder/payment.py
+++
@@ -1,9 +0,0 @@
-import os
-from payos import PayOS
-
-
-payos = PayOS(
-    client_id=os.getenv("PAYOS_CLIENT_ID"),
-    api_key=os.getenv("PAYOS_API_KEY"),
-    checksum_key=os.getenv("PAYOS_CHECKSUM_KEY")
-)
