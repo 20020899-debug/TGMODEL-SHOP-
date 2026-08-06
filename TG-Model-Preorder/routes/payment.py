da64d19a3a49dc07c105bcbeff85ef52e1f1e848
20020899-debug <20020899@vnu.edu.vn>
2026-08-06 09:24:17 +0700

    Update payment.py

--- TG-Model-Preorder/routes/payment.py
+++ TG-Model-Preorder/routes/payment.py
@@ -1,9 +1,5 @@
 from flask import Blueprint, render_template, request
 import sqlite3
-import os
-import hmac
-import hashlib
-import json
 
 
 payment_bp = Blueprint(
@@ -49,54 +45,110 @@ def payment_cancel():
 )
 def webhook():
 
+    print("==============================")
+    print("PAYOS WEBHOOK RECEIVED")
+    print("==============================")
+
+
     data = request.json
 
 
+    print("DATA:")
+    print(data)
+
+
+
     if not data:
+
+        print("NO DATA")
 
         return "No data", 400
 
 
 
-    # PayOS gửi trạng thái thành công code = 00
+    # PayOS thanh toán thành công
 
     if data.get("code") == "00":
 
 
-        order_code = data["data"]["orderCode"]
+        try:
 
 
-        order_code = f"TG{order_code:06d}"
+            payos_order_code = data["data"]["orderCode"]
 
 
+            # Đổi sang mã đơn của shop
+            # 1 -> TGM001
+            # 2 -> TGM002
 
-        conn = sqlite3.connect(
-            "orders.db"
-        )
-
-        cursor = conn.cursor()
-
+            order_code = f"TGM{payos_order_code:03d}"
 
 
-        cursor.execute(
-            """
-            UPDATE orders
-
-            SET status=?
-
-            WHERE order_code=?
-
-            """,
-            (
-                "Đã cọc",
+            print(
+                "UPDATE ORDER:",
                 order_code
             )
+
+
+
+            conn = sqlite3.connect(
+                "orders.db"
+            )
+
+
+            cursor = conn.cursor()
+
+
+
+            cursor.execute(
+                """
+                UPDATE orders
+
+                SET status=?
+
+                WHERE order_code=?
+
+                """,
+                (
+                    "Đã cọc",
+                    order_code
+                )
+            )
+
+
+
+            print(
+                "UPDATED ROW:",
+                cursor.rowcount
+            )
+
+
+
+            conn.commit()
+
+            conn.close()
+
+
+
+        except Exception as e:
+
+
+            print(
+                "WEBHOOK ERROR:",
+                e
+            )
+
+
+            return "Error", 500
+
+
+
+    else:
+
+
+        print(
+            "PAYMENT NOT SUCCESS"
         )
-
-
-
-        conn.commit()
-        conn.close()
 
 
 
