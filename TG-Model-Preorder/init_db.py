5f36d3b2d5900e25719b0e4dab9b7a88399348df
20020899-debug <20020899@vnu.edu.vn>
2026-08-07 13:24:33 +0700

    Update init_db.py

--- TG-Model-Preorder/init_db.py
+++ TG-Model-Preorder/init_db.py
@@ -1,45 +1,45 @@
 from database import get_db
 
 conn = get_db()
+
 cursor = conn.cursor()
 
-# Tạo bảng nếu chưa có
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS orders (
 
-    id SERIAL PRIMARY KEY,
+id SERIAL PRIMARY KEY,
 
-    order_code TEXT,
+order_code TEXT,
 
-    fullname TEXT,
-    phone TEXT,
-    contact TEXT,
+fullname TEXT,
+phone TEXT,
+contact TEXT,
 
-    province TEXT,
-    district TEXT,
-    ward TEXT,
-    address_detail TEXT,
+province TEXT,
+district TEXT,
+ward TEXT,
+address_detail TEXT,
 
-    quantity INTEGER,
+quantity INTEGER,
 
-    note TEXT,
+note TEXT,
 
-    product_name TEXT,
-    product_brand TEXT,
+product_name TEXT,
+product_brand TEXT,
 
-    price INTEGER,
-    deposit INTEGER,
+price INTEGER,
+deposit INTEGER,
 
-    status TEXT,
+status TEXT,
 
-    created_at TEXT
+created_at TEXT,
+expires_at TIMESTAMP,
+
+payment_url TEXT,
+order_token TEXT
+```
+
 )
-""")
-
-# Thêm cột expires_at nếu chưa có
-cursor.execute("""
-ALTER TABLE orders
-ADD COLUMN IF NOT EXISTS expires_at TIMESTAMP;
 """)
 
 conn.commit()
