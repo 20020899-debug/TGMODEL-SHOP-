ce3d5404ca17517123d0688b67a1eacb0927654a
20020899-debug <20020899@vnu.edu.vn>
2026-08-07 08:42:23 +0700

    Update preorder.py

--- TG-Model-Preorder/routes/preorder.py
+++ TG-Model-Preorder/routes/preorder.py
@@ -7,7 +7,6 @@ from database import get_db
 from config import products
 from payos_service import payos
 
-
 preorder_bp = Blueprint(
     "preorder",
     __name__
@@ -56,7 +55,7 @@ def submit():
 
 
     # =========================
-    # Kết nối database
+    # Kết nối Database
     # =========================
 
     conn = get_db()
@@ -64,15 +63,13 @@ def submit():
 
 
     # =========================
-    # Tạo mã đơn
+    # Tạo mã đơn shop
     # =========================
 
-    cursor.execute(
-        """
-        SELECT COALESCE(MAX(id),0)
+    cursor.execute("""
+        SELECT COALESCE(MAX(id), 0)
         FROM orders
-        """
-    )
+    """)
 
     count = cursor.fetchone()[0] + 1
 
@@ -85,23 +82,27 @@ def submit():
 
     payos_order_code = int(time.time())
 
+
+    # =========================
+    # Thời gian
+    # =========================
+
     created_time = datetime.now(
-    ZoneInfo("Asia/Ho_Chi_Minh")
-)
+        ZoneInfo("Asia/Ho_Chi_Minh")
+    )
 
-created_at = created_time.strftime(
-    "%d/%m/%Y %H:%M:%S"
-)
+    created_at = created_time.strftime(
+        "%d/%m/%Y %H:%M:%S"
+    )
 
-expires_at = created_time + timedelta(minutes=30)
+    expires_at = created_time + timedelta(minutes=30)
 
 
     # =========================
     # Lưu đơn hàng
     # =========================
 
-    cursor.execute(
-        """
+    cursor.execute("""
         INSERT INTO orders
         (
             order_code,
@@ -122,40 +123,40 @@ expires_at = created_time + timedelta(minutes=30)
             created_at,
             expires_at
         )
-
         VALUES
         (
-            %s,%s,%s,%s,%s,%s,%s,%s,
-            %s,%s,%s,%s,%s,%s,%s,%s
+            %s, %s, %s, %s,
+            %s, %s, %s, %s,
+            %s, %s, %s, %s,
+            %s, %s, %s, %s, %s
         )
-        """,
-        (
-            order_code,
-            fullname,
-            phone,
-            contact,
-            province,
-            district,
-            ward,
-            address_detail,
-            quantity,
-            note,
-            product["name"],
-            product["brand"],
-            product["price"],
-            product["deposit"],
-            "Chưa thanh toán",
-            created_at,
-            expires_at
-        )
-    )
+    """,
+    (
+        order_code,
+        fullname,
+        phone,
+        contact,
+        province,
+        district,
+        ward,
+        address_detail,
+        quantity,
+        note,
+        product["name"],
+        product["brand"],
+        product["price"],
+        product["deposit"],
+        "Chưa thanh toán",
+        created_at,
+        expires_at
+    ))
 
     conn.commit()
     conn.close()
 
 
     # =========================
-    # Tạo link PayOS
+    # Tạo link thanh toán PayOS
     # =========================
 
     payment_data = {
@@ -163,12 +164,17 @@ expires_at = created_time + timedelta(minutes=30)
         "amount": product["deposit"] * quantity,
         "description": order_code,
         "returnUrl": "https://tgmodel-shop.onrender.com/payment/success",
-        "cancelUrl": "https://tgmodel-shop.onrender.com/payment/cancel"
+        "cancelUrl": f"https://tgmodel-shop.onrender.com/payment/cancel?order={order_code}"
     }
 
     payment_link = payos.payment_requests.create(
         payment_data
     )
+
+
+    # =========================
+    # Chuyển sang PayOS
+    # =========================
 
     return redirect(
         payment_link.checkout_url
