d5687d75a9748e7e5b7724dcd5fe8b47957fa87b
20020899-debug <20020899@vnu.edu.vn>
2026-08-10 18:34:24 +0700

    Delete TG-Model-Preorder directory

--- TG-Model-Preorder/payos_service.py
+++
@@ -1,8 +0,0 @@
-from payos import PayOS
-import os
-
-payos = PayOS(
-    client_id=os.getenv("PAYOS_CLIENT_ID"),
-    api_key=os.getenv("PAYOS_API_KEY"),
-    checksum_key=os.getenv("PAYOS_CHECKSUM_KEY")
-)
