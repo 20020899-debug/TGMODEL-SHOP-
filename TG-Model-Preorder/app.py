5444228252ed47dfba0ad4f8be30b10f4ec6be6e
20020899-debug <20020899@vnu.edu.vn>
2026-08-10 10:20:39 +0700

    Update app.py

--- TG-Model-Preorder/app.py
+++ TG-Model-Preorder/app.py
@@ -14,6 +14,7 @@ from routes.submit_order import submit_order_bp
 from routes.payment_success import payment_success_bp
 from routes.payment_cancel import payment_cancel_bp
 from routes.payment_webhook import payment_webhook_bp
+from routes.admin_stock import admin_stock_bp
 
 app.register_blueprint(home_bp)
 app.register_blueprint(auth_bp)
@@ -24,6 +25,7 @@ app.register_blueprint(submit_order_bp)
 app.register_blueprint(payment_success_bp)
 app.register_blueprint(payment_cancel_bp)
 app.register_blueprint(payment_webhook_bp)
+app.register_blueprint(admin_stock_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
