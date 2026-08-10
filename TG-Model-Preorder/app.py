36caec9fdc508a6af96a0ef5ae369c48d7b0297c
20020899-debug <20020899@vnu.edu.vn>
2026-08-10 08:10:11 +0700

    Update app.py

--- TG-Model-Preorder/app.py
+++ TG-Model-Preorder/app.py
@@ -7,7 +7,6 @@ app.secret_key = SECRET_KEY
 
 from routes.home import home_bp
 from routes.auth import auth_bp
-from routes.preorder import preorder_bp
 from routes.admin import admin_bp
 from routes.payment import payment_bp
 from routes.pending_order import pending_order_bp
@@ -16,7 +15,6 @@ from routes.submit_order import submit_order_bp
 
 app.register_blueprint(home_bp)
 app.register_blueprint(auth_bp)
-app.register_blueprint(preorder_bp)
 app.register_blueprint(admin_bp)
 app.register_blueprint(payment_bp)
 app.register_blueprint(pending_order_bp)
